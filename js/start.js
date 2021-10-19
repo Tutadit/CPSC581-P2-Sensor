58e3e6818df18b86ff535aff2ec46d925d6d6816
Juan Pablo Lozano Sarmiento <jp@osloz.com>
2021-10-19 10:27:04 -0600

    One more

--- js/start.js
+++ js/start.js
@@ -2,12 +2,13 @@ import { getSimon, Direction } from "./simonSays.js";
 import { getOrientationSensor } from "./sensors.js"
 
 
-function handleDirection(direction) {
-  simon.activate(direction)
-}
+
 
 $(document).ready(function () {
   let simon = getSimon();
+  function handleDirection(direction) {
+    simon.activate(direction)
+  }
   let orientationSensor = getOrientationSensor(handleDirection);
   $("#getPermision").click(function () {
       orientationSensor.start()
