8b2f0386235a21c2cbe0fdfc7a0b8407f7fd5bc8
Juan Pablo Lozano Sarmiento <jp@osloz.com>
2021-10-19 10:29:38 -0600

    way too

--- js/start.js
+++ js/start.js
@@ -1,18 +1,17 @@
 import { getSimon, Direction } from "./simonSays.js";
-import { getOrientationSensor } from "./sensors.js"
+import { getOrientationSensor } from "./sensors.js";
 
-
-
+function handleDirection(direction) {
+  console.log(direction);
+  this.activate(direction);
+}
 
 $(document).ready(function () {
   let simon = getSimon();
-  function handleDirection(direction) {
-    console.log(direction)
-    this.activate(direction)
-  }
-  let directionHandler = handleDirection.bind(simon)
+
+  let directionHandler = handleDirection.bind(simon);
   let orientationSensor = getOrientationSensor(directionHandler);
   $("#getPermision").click(function () {
-      orientationSensor.start()
-  });  
+    orientationSensor.start();
+  });
 });
