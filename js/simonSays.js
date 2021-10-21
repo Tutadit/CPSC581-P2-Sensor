602bfa5cab777abc20005106a81725e0e6447ffb
SofianM17 <47373409+SofianM17@users.noreply.github.com>
2021-10-20 22:23:10 -0600

    Add files via upload

--- js/simonSays.js
+++ js/simonSays.js
@@ -121,9 +121,7 @@ class SimonSays {
     // TODO: Notify the user that the pattern entered is incorrect.
     // Reset the system. use this.reset()
     // Use this.blocks.message to write a message to user.
-    this.blocks.message.text("Incorrect, please try again");
-    setTimeout(function(){
-      this.blocks.message.text("");}, 2000);
+    this.blocks.message.text("Incorrect, please try again")
     this.reset();
   }
 
